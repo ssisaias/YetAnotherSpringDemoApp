d4356dadeff519444f85e8658d90b6b1fb3f0d64
Isaias S. Silva <luiz.isaias95@gmail.com>
2017-07-23 18:27:21 -0300

    Allow cors

---
+++ src/main/java/br/ufc/demo1/config/RestConfiguration.java
@@ -0,0 +1,26 @@
+package br.ufc.demo1.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        //https://stackoverflow.com/questions/31724994/spring-data-rest-and-cors
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
